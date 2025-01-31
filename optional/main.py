91f485cd68b12801972feb742d5a7a9a43532804
kenberu <kenberu.development@gmail.com>
2025-01-31 15:47:08 +0900

    optional型の演習

---
+++ optional/main.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+def get_profile(
+    email: str,
+    username: Optional[str] = None,
+    age: Optional[int] = None,
+) -> dict:
+    profile = {"email": email}
+    if username:
+        profile["username"] = username
+    if age:
+        profile["age"] = age
+    return profile
+
+user_profile = get_profile(email="user@example.com")
+print(user_profile)
+
+complete_profile = get_profile(email="user@example.com", username="元太", age=30)
+print(complete_profile)
